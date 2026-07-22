656999d97c7e9a525f7b0a5a13d006d7e5e501b8
Naveen02052005 <naveenmerugu959@gmail.com>
2026-07-22 16:07:49 +0530

    Final Commit

--- config/mailer.js
+++ config/mailer.js
@@ -1,15 +1,30 @@
+// const nodemailer = require("nodemailer");
+
+// console.log("========== MAILER LOADED ==========");
+// console.log("EMAIL_USER:", process.env.EMAIL_USER);
+// console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// module.exports = transporter;
+
 const nodemailer = require("nodemailer");
 
-console.log("========== MAILER LOADED ==========");
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,   // Force IPv4
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 module.exports = transporter;
