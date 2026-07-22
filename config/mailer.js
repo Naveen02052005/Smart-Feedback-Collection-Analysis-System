20f0f13e7a7d560bb94ab17345e6524c6ddf2f3a
Naveen02052005 <naveenmerugu959@gmail.com>
2026-07-22 16:51:19 +0530

    Final Commit

--- config/mailer.js
+++ config/mailer.js
@@ -1,16 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
-
     auth: {
         user: process.env.BREVO_USER,
         pass: process.env.BREVO_SMTP_KEY
     }
-
 });
 
 module.exports = transporter;
